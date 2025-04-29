f7242262a5149c8510951df26ba4c90991a0c9fd
Justin Gery <jaygery90@gmail.com>
2025-04-28 22:54:04 -0500

    fixed error

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -6,9 +6,7 @@ import Layout from "../components/Layout";
 export default function Shop() {
     return (
         <Layout>
-            <section>
-                <h1 className="text-white">Shop</h1>
-            </section>
+            
         </Layout>
     )
 };
